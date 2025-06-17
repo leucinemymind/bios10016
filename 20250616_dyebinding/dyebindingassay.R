3a3abc092011128b6708f973af7179bbf3545ce4
leucinemymind <emilyy2009x@gmail.com>
2025-06-17 08:57:14 -0500

    more cleanup + fundamentals

--- 20250616_dyebinding/dyebindingassay.R
+++ 20250616_dyebinding/dyebindingassay.R
@@ -2,17 +2,17 @@
 library(stats)
 
 # x and y axes
-protein_concentration_ul <- c(0,10,20,30,40,60)
+protein_mass_ul <- c(0,10,20,30,40,60)
 absorbance <- c(0, 0.099, 0.263, 0.407, 0.504, 0.684)
 
 # plot
-plot(protein_concentration_ul, absorbance, col = "blue", pch = 19,
+plot(protein_mass_ul, absorbance, col = "blue", pch = 19,
      xlab = "Protein mass (µg)", ylab = "Absorbance",
      main = "Absorbance vs protein mass",)
 
 # linear regression
 
-assay_lm <- lm(absorbance ~ protein_concentration_ul)
+assay_lm <- lm(absorbance ~ protein_mass_ul)
 abline(assay_lm)
 coef(assay_lm) # y = 0.0118*x + 0.0122
 
@@ -20,18 +20,18 @@ coef(assay_lm) # y = 0.0118*x + 0.0122
 summary(assay_lm) #R^2 = 0.9821
 
 # interpolation - function
-interpolate_protein_concentration <- function(absorbance_value) {
+interpolate_protein_mass <- function(absorbance_value) {
   # Calculate the protein concentration for a given absorbance value
   assay_intercept <- coef(assay_lm)[1]
   assay_slope <- coef(assay_lm)[2]
   
   # Solve for x in y = mx + b
-  protein_concentration <- (absorbance_value - assay_intercept) / assay_slope
-  return(protein_concentration)
+  protein_mass <- (absorbance_value - assay_intercept) / assay_slope
+  return(protein_mass)
 }
 
 # values
 
-interpolate_protein_concentration(0.309) #mass = 25.21 micrograms
-interpolate_protein_concentration(0.440) #mass = 36.33 micrograms
-interpolate_protein_concentration(0.433) # mass = 35.74 micrograms
+interpolate_protein_mass(0.309) #mass = 25.21 micrograms
+interpolate_protein_mass(0.440) #mass = 36.33 micrograms
+interpolate_protein_mass(0.433) # mass = 35.74 micrograms
